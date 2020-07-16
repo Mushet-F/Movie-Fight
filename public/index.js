c71c2739af390225a2b4d240dca4dd7e88936355
Mushet F <frasermushet@live.co.uk>
2020-07-16 11:04:38 +0100

    https

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ const autocompleteConfig = {
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: 'b844b798',
             s: searchTerm
@@ -46,7 +46,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: 'b844b798',
             i: movie.imdbID
